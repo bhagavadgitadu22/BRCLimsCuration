251ee583a9d1b79c7bb1e20aca7730e483e62e4d
Martin  BOUTROUX <martin.boutroux@pasteur.fr>
2021-08-23 16:29:09 +0200

    Deal with the strains having a sch_lieu_precis but no sch_lieu.

---
+++ localisation/attempt/avec_lieu_precis_sans_lieu.sql
@@ -0,0 +1,15 @@
+SELECT sch_identifiant, sch_lieu, sch_lieu_precis 
+FROM t_souche 
+LEFT JOIN t_donneedico
+ON t_donneedico.xxx_id = sch_lieu
+WHERE t_donneedico.xxx_id IS NULL
+AND sch_lieu_precis IS NOT NULL
+AND sch_lieu_precis != ''
+AND NOT(sch_identifiant LIKE '%SB%'
+OR sch_identifiant LIKE '%CIP%'
+OR sch_identifiant LIKE '%CRBIP%')
+
+-- 1889 SB
+-- 32 CIP ou CRBIP
+-- 5 PCC
+-- 2 trucs bizarres C7 et G9
