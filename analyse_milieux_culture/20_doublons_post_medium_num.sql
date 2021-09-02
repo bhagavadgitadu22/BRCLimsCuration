d7be32aba4d87b92411ec07600021dd3fec3f12f
Martin  BOUTROUX <mboutrou@pasteur.fr>
2021-09-02 15:17:45 +0200

    Test with json aggregates.

--- analyse_milieux_culture/20_doublons_post_medium_num.sql
+++ analyse_milieux_culture/20_doublons_post_medium_num.sql
@@ -1,10 +1,25 @@
-SELECT ARRAY_AGG(xxx_id), ARRAY_AGG(mil_designation_en), arr FROM
+SELECT ARRAY_AGG(xxx_id), ARRAY_AGG(sch_par_milieu_json), arr FROM
 
-(SELECT xxx_id, mil_designation_en, (REGEXP_MATCHES(mil_designation_en, 'MEDIUM [0-9]+ - (.*)'))[1] as arr
-FROM t_milieu
+(SELECT xxx_id, 
+(REGEXP_MATCHES(mil_designation_en, 'MEDIUM [0-9]+ - (.*)'))[1] as arr,
+sch_par_milieu_json
+
+FROM 
+ 
+(SELECT t_milieu.xxx_id, mil_designation_en, mil_clg_id, 
+jsonb_build_object(mil_designation_en, ARRAY_AGG(sch_identifiant)) AS sch_par_milieu_json
+FROM 
+t_milieu
+LEFT JOIN t_milieu_souche
+ON msc_mil_id = t_milieu.xxx_id
+LEFT JOIN t_souche
+ON t_souche.xxx_id = msc_sch_id
+
+GROUP BY t_milieu.xxx_id, mil_designation_en, mil_clg_id) AS schs_par_milieu
+ 
 WHERE mil_clg_id = 401
 AND mil_designation_en SIMILAR TO '%MEDIUM [0-9]+ -%') AS a
 
 GROUP BY arr
 HAVING COUNT(*) > 1
-ORDER BY ARRAY_AGG(mil_designation_en);
+ORDER BY ARRAY_AGG(sch_par_milieu_json);
--- analyse_milieux_culture/30_repetition_ingredients.sql
+++ analyse_milieux_culture/30_repetition_ingredients.sql
@@ -1,15 +1,28 @@
-SELECT ARRAY_AGG(xxx_id), ARRAY_AGG(mil_designation_en), arr FROM 
+SELECT ARRAY_AGG(xxx_id), ARRAY_AGG(sch_par_milieu_json), arr FROM 
 
-(SELECT t_milieu.xxx_id, mil_designation_en,
-ARRAY_AGG(cmp_designation_en ORDER BY cmp_designation_en) AS arr
-FROM t_milieu
+(SELECT schs_par_milieu.xxx_id, 
+ARRAY_AGG(cmp_designation_en ORDER BY cmp_designation_en) AS arr,
+sch_par_milieu_json
+FROM 
+ 
+(SELECT t_milieu.xxx_id, mil_designation_en, mil_clg_id, 
+jsonb_build_object(mil_designation_en, ARRAY_AGG(sch_identifiant)) AS sch_par_milieu_json
+FROM 
+t_milieu
+LEFT JOIN t_milieu_souche
+ON msc_mil_id = t_milieu.xxx_id
+LEFT JOIN t_souche
+ON t_souche.xxx_id = msc_sch_id
+
+GROUP BY t_milieu.xxx_id, mil_designation_en, mil_clg_id) AS schs_par_milieu
+
 JOIN t_milieu_composition
-ON lmc_mil_id = t_milieu.xxx_id
+ON lmc_mil_id = schs_par_milieu.xxx_id
 JOIN t_composition
 ON lmc_cmp_id = t_composition.xxx_id
 WHERE mil_clg_id = 401
-GROUP BY t_milieu.xxx_id, mil_designation_en) AS a
+GROUP BY schs_par_milieu.xxx_id, sch_par_milieu_json) AS a
 
 GROUP BY arr
 HAVING COUNT(*) > 1
-ORDER BY ARRAY_AGG(mil_designation_en);
+ORDER BY ARRAY_AGG(sch_par_milieu_json);
