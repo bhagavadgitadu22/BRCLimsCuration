f049d047a2ab447b66007576731bd17a5aa905a1
Martin  BOUTROUX <martin.boutroux@pasteur.fr>
2021-09-16 11:28:06 +0200

    Problem with balancelles solved hopefully.

---
+++ curation/30_balancelles/26_emplacements_libres_par_balancelle_stockeur_1.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS id_boite_180;
+DROP TABLE IF EXISTS emplacements_libres_stockeur_1;
+
+SELECT cst_tst_id
+INTO id_boite_180
+FROM t_casestockage
+JOIN t_lot_casestockage
+ON t_casestockage.xxx_id = lts_cst_id
+JOIN ids_balancelles_1
+ON ids_balancelles_1.xxx_id = lts_lst_id
+GROUP BY cst_tst_id;
+
+SELECT id_balancelle, lst_nom, 
+id_case, cst_numero, cst_pos_x, cst_pos_y,
+ROW_NUMBER() OVER (PARTITION BY id_balancelle) AS numero_case
+INTO emplacements_libres_stockeur_1
+FROM
+(SELECT ids_balancelles_1.xxx_id AS id_balancelle, lst_nom, 
+t_casestockage.xxx_id AS id_case, cst_numero, cst_pos_x, cst_pos_y
+FROM ids_balancelles_1, t_casestockage
+WHERE cst_tst_id IN (SELECT * FROM id_boite_180)
+EXCEPT
+SELECT ids_balancelles_1.xxx_id, lst_nom, 
+t_casestockage.xxx_id, cst_numero, cst_pos_x, cst_pos_y
+FROM t_casestockage
+JOIN t_lot_casestockage
+ON t_casestockage.xxx_id = lts_cst_id
+JOIN ids_balancelles_1
+ON ids_balancelles_1.xxx_id = lts_lst_id) AS a
+ORDER BY lst_nom, cst_numero;
+
+DROP TABLE IF EXISTS id_boite_180;
--- curation/30_balancelles/60_update_stockeur.sql
+++ curation/30_balancelles/60_update_stockeur.sql
@@ -1,11 +1,17 @@
 UPDATE t_lot_casestockage
-SET lts_lst_id = id_balancelle,
-	lts_cst_id = (SELECT xxx_id FROM t_casestockage WHERE cst_numero = 110+nb_row AND cst_tst_id = 4647877)
+SET lts_lst_id = lots_et_balancelles_1.id_balancelle,
+	lts_cst_id = numero_case
 FROM lots_et_balancelles_1
-WHERE lts_lot_id = lot_id;
+JOIN emplacements_libres_stockeur_1
+ON emplacements_libres_stockeur_1.id_balancelle = lots_et_balancelles_1.id_balancelle
+WHERE lts_lot_id = lot_id
+AND numero_case = nb_row;
 
 UPDATE t_lot_casestockage
-SET lts_lst_id = id_balancelle,
-	lts_cst_id = (SELECT xxx_id FROM t_casestockage WHERE cst_numero = 110+nb_row AND cst_tst_id = 4647877)
+SET lts_lst_id = lots_et_balancelles_1.id_balancelle,
+	lts_cst_id = numero_case
 FROM lots_et_balancelles_2
-WHERE lts_lot_id = lot_id;
+JOIN emplacements_libres_stockeur_2
+ON emplacements_libres_stockeur_2.id_balancelle = lots_et_balancelles_2.id_balancelle
+WHERE lts_lot_id = lot_id
+AND numero_case = nb_row;
