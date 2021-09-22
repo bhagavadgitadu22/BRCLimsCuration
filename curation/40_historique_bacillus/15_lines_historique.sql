853cfc2bc0decc196616b983125f0cd9ab16b6e8
Martin  BOUTROUX <mboutrou@pasteur.fr>
2021-09-22 17:47:32 +0200

    One line was missing to create lines_bacillus.

--- curation/40_historique_bacillus/15_lines_historique.sql
+++ curation/40_historique_bacillus/15_lines_historique.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS lines_bacillus;
 
 SELECT xxx_id, numero, line, max(annee::integer) AS bonne_annee, full_trim((regexp_matches(line, CONCAT(max(annee::integer), '(.*)'), 'g'))[1]) AS texte
-FROM 
+INTO lines_bacillus FROM 
 
 (SELECT xxx_id, numero, line, (regexp_matches(line, '[0-9]{4}', 'g'))[1] AS annee FROM
 
