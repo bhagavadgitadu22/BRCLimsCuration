1a4c6016d13de1bc0465feeb35ce4f82a6e53e01
Dominique BOUTROUX <martinboutroux@outlook.fr>
2021-12-29 11:05:34 +0100

    End of wdcm stats + tryout of script to link souches types and souches normales.

---
+++ analyse/nagoya/pays_restants_dans_brclims.sql
@@ -0,0 +1,11 @@
+SELECT don_lib, COUNT(*)
+FROM t_souche
+JOIN t_donneedico
+ON t_donneedico.xxx_id = sch_lieu
+WHERE t_souche.xxx_id IN (SELECT xxx_id FROM souches_groupe_cip)
+AND don_lib NOT LIKE '%Korea%'
+AND don_lib NOT SIMILAR TO '(1964|CEI|Yugoslavia|USSR)'
+AND don_lib NOT SIMILAR TO '(Africa|Asia|Europe|South America|Arctic|Antarctica)'
+AND don_lib NOT SIMILAR TO '%(Sea|Ocean)%'
+GROUP BY don_lib
+ORDER BY don_lib;
--- python/divers/wdcm_mariana_dom.py
+++ python/divers/wdcm_mariana_dom.py
@@ -1,11 +1,10 @@
 import pandas as pd
+from openpyxl.workbook.workbook import Workbook
 import psycopg2
 import re
 
 wdcm_normes = pd.ExcelFile(r"e:\Users\Proprietaire\Downloads\Normes et WDM souches_DC.xlsx")
-wdcm_mariana = pd.ExcelFile(r'e:\Users\Proprietaire\Downloads\211227_WDCM_Panel_MF.xlsx')
 
-# je lis mon fichier ligne par ligne pour choper les infos des identifiants, pour sheet 1 puis 2
 df_normes = pd.read_excel(wdcm_normes, "Feuil1")
 
 normes_eclates = {}
@@ -21,7 +20,8 @@ for i in range(len(df_normes)):
     for wdcm in wdcms:
         if wdcm != '':
             res = prog.search(wdcm)
-            short_wdcm = 'WDCM '+str(int(res.group(1)))
+            # short_wdcm = 'WDCM '+str(int(res.group(1)))
+            short_wdcm = wdcm
             
             if short_wdcm not in normes_eclates:
                 normes_eclates[short_wdcm] = []
@@ -33,6 +33,54 @@ for i in range(len(df_normes)):
             if test not in tests_eclates[short_wdcm]:   
                 tests_eclates[short_wdcm].append(test)
 
-print(normes_eclates)
-print(tests_eclates)
+# print(normes_eclates)
+# print(tests_eclates)
+
+wdcm_mariana = pd.ExcelFile(r'e:\Users\Proprietaire\Downloads\211227_WDCM_Panel_MF.xlsx')
+
+df_mar = pd.read_excel(wdcm_mariana, "Feuil1")
+
+connection = psycopg2.connect(user="postgres",
+                                password="hercule1821",
+                                host="localhost",
+                                port="5432",
+                                database="brc_db")
+connection.autocommit = True
+
+cursor = connection.cursor()
+
+wb = Workbook()
+sheet = wb.create_sheet("name")
+sheet.append(["id", "wdcm", "normes", "tests", "commandes 10 ans"])
+
+for i in range(len(df_mar)):
+    souche_cip = df_mar.iloc[i, 0]
+    if not(pd.isnull(souche_cip)):
+        # print(souche_cip)
+
+        str_sql = open("../analyse/corynebacterium/get_wdcm.sql", "r", encoding='utf-8').read()
+        complement_sql = " WHERE (t_commande.xxx_id IS NULL OR EXTRACT(YEAR FROM cmd_dat) > 2010) AND sch_identifiant = '"+souche_cip+"' GROUP BY sch_identifiant, (REGEXP_MATCHES(sch_references_equi, 'WDCM ?([0-9]+)'))[1];"
+
+        cursor.execute(str_sql+complement_sql)
+        record = cursor.fetchone()
+
+        row = [souche_cip]
+        if record is not None:
+            local_wdcm = 'WDCM '+record[1]
+            local_norme = ''
+            local_test = ''
+            if local_wdcm in normes_eclates:
+                local_norme = '\n'.join(normes_eclates[local_wdcm])
+            if local_wdcm in tests_eclates:
+                local_test = '\n'.join(tests_eclates[local_wdcm])
+
+            row = [souche_cip, local_wdcm, local_norme, local_test, record[2]]
+            # print(row)
+        if record is None:
+            print(souche_cip)
+        
+        sheet.append(row)
+
+del wb["Sheet"]
+wb.save(r"C:\Users\Public\Documents\bilan_wdcm_mariana.xlsx")
 
