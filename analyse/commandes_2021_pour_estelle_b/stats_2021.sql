098151ef0ff62e227b3a981d79c12b2132308f0a
calvin <martinboutroux@outlook.fr>
2022-01-25 18:00:55 +0100

    Stats pour Estelle M.

--- analyse/commandes_2021_pour_estelle_b/stats_2021.sql
+++ analyse/commandes_2021_pour_estelle_b/stats_2021.sql
@@ -1,5 +1,5 @@
-SELECT cmd_num_bon_livraison, cmd_dat, clt_fact_raison_sociale, code_tarif, array_to_string(ARRAY_AGG(qte), ', ') FROM
-(SELECT cmd_num_bon_livraison, cmd_dat, clt_fact_raison_sociale, don_lib AS code_tarif, CONCAT(sch_identifiant, ': ', COUNT(*)) AS qte
+SELECT cmd_num_bon_livraison, cmd_dat, cmd_exportee_dat, clt_fact_raison_sociale, code_tarif, array_to_string(ARRAY_AGG(qte), ', ') FROM
+(SELECT cmd_num_bon_livraison, cmd_dat, cmd_exportee_dat, clt_fact_raison_sociale, don_lib AS code_tarif, CONCAT(sch_identifiant, ': ', COUNT(*)) AS qte
 FROM t_commande
 LEFT JOIN t_client
 ON cmd_clt_id = t_client.xxx_id
@@ -11,7 +11,7 @@ LEFT JOIN t_lot
 ON acm_lot_id = t_lot.xxx_id
 LEFT JOIN t_souche 
 ON lot_sch_id = t_souche.xxx_id
-WHERE EXTRACT(YEAR FROM cmd_dat) = 2021
+WHERE EXTRACT(YEAR FROM cmd_exportee_dat) = 2021
 AND cmd_envoi_gratuit IS True
-GROUP BY cmd_num_bon_livraison, cmd_dat, clt_fact_raison_sociale, code_tarif, sch_identifiant) AS a
-GROUP BY cmd_num_bon_livraison, cmd_dat, clt_fact_raison_sociale, code_tarif;
+GROUP BY cmd_num_bon_livraison, cmd_dat, cmd_exportee_dat, clt_fact_raison_sociale, code_tarif, sch_identifiant) AS a
+GROUP BY cmd_num_bon_livraison, cmd_dat, cmd_exportee_dat, clt_fact_raison_sociale, code_tarif;
