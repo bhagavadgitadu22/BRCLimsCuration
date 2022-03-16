d32376ac5b06b4b8b16c5281bb0df8dbe65d4998
Martin  BOUTROUX <mboutrou@pasteur.fr>
2022-03-16 15:27:46 +0100

    Finally it works (i think :/).

--- python/move_genomes_estelle_m/45_sanity_check.py
+++ python/move_genomes_estelle_m/45_sanity_check.py
@@ -7,14 +7,15 @@ xls = pd.ExcelFile('../../output/transfert_infos_p2m.xlsx')
 #pos_genome = 5
 pos_genome = 2
 
-list_ids = []
-df = pd.read_excel(xls, 'genomes')
-df = df.reset_index(drop=True)  # make sure indexes pair with number of rows
-for index, row in df.iterrows():
-    if row[1].split('/')[pos_genome] not in list_ids:
-        list_ids.append(row[1].split('/')[pos_genome])
+# list_ids = []
+# df = pd.read_excel(xls, 'genomes')
+# df = df.reset_index(drop=True)  # make sure indexes pair with number of rows
+# for index, row in df.iterrows():
+#     if row[1].split('/')[pos_genome] not in list_ids:
+#         list_ids.append(row[1].split('/')[pos_genome])
 
-sub_ids = list_ids[:10]
+# sub_ids = list_ids[:10]
+sub_ids = ['111770T', '55.117', '102968T', '103611T', '103959', '105187', '105188']
 
 print(sub_ids)
 
@@ -26,10 +27,13 @@ for index, row in df.iterrows():
     if id in sub_ids:
         old_location = row[0]
         new_location = row[1]
+
+        #print('')
+        #print(old_location)
+        #print(new_location)
+
         if not(os.path.exists(old_location)):
-            print('')
-            print(old_location)
-            print(new_location)
+            print('aie')
 
 df = pd.read_excel(xls, 'genomes')
 df = df.reset_index(drop=True)  # make sure indexes pair with number of rows
@@ -39,7 +43,10 @@ for index, row in df.iterrows():
     if id in sub_ids:
         old_location = row[0]
         new_location = row[1]
+
+        # print('')
+        # print(old_location)
+        # print(new_location)
+
         if not(os.path.exists(old_location)):
-            print('')
-            print(old_location)
-            print(new_location)
+            print('aie')
--- python/move_genomes_estelle_m/50_transfert_effectif.py
+++ python/move_genomes_estelle_m/50_transfert_effectif.py
@@ -2,21 +2,56 @@ import pandas as pd
 import os
 import shutil
 
+from openpyxl.workbook.workbook import Workbook
+from openpyxl.styles import Border, Side, Font
+
+def redimension_cell_width(ws):
+    dims = {}
+    for row in ws.rows:
+        for cell in row:
+            if cell.value:
+                line_max = max([len(str(elmt)) for elmt in str(cell.value).split('\n')])
+                max_ = max((dims.get(cell.column_letter, 0), line_max))
+                dims[cell.column_letter] = max_
+    for col, value in dims.items():
+        ws.column_dimensions[col].width = value
+
+def style_sheet(sheet):
+    header = list(sheet.rows)[0]
+    for cell in header:
+        cell.font  = Font(bold=True)
+    
+    redimension_cell_width(sheet)
+    borders_cells(sheet)
+
+def borders_cells(sheet):
+    thin = Side(border_style="thin", color="000000")
+
+    for col in sheet.rows:
+        for cell in col:
+            if cell.value:
+                cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
+
 xls = pd.ExcelFile('../../output/transfert_infos_p2m.xlsx')
 
 #pos_genome = 5
 pos_genome = 2
 
-list_ids = []
-df = pd.read_excel(xls, 'genomes')
-df = df.reset_index(drop=True)  # make sure indexes pair with number of rows
-for index, row in df.iterrows():
-    if row[1].split('/')[pos_genome] not in list_ids:
-        list_ids.append(row[1].split('/')[pos_genome])
+# list_ids = []
+# df = pd.read_excel(xls, 'genomes')
+# df = df.reset_index(drop=True)  # make sure indexes pair with number of rows
+# for index, row in df.iterrows():
+#     if row[1].split('/')[pos_genome] not in list_ids:
+#         list_ids.append(row[1].split('/')[pos_genome])
 
-sub_ids = list_ids[:10]
+# sub_ids = list_ids[:10]
+sub_ids = ['111770T', '55.117', '102968T', '103611T', '103959', '105187', '105188']
 
 print(sub_ids)
+
+dirs_created = []
+metafiles_moved = []
+genomes_moved = []
 
 df = pd.read_excel(xls, 'directories')
 df = df.reset_index(drop=True)  # make sure indexes pair with number of rows
@@ -27,6 +62,7 @@ for index, row in df.iterrows():
         dir = row[0]
         if not(os.path.exists(dir)):
             os.mkdir(dir)
+            dirs_created.append(dir)
         else:
             print('')
             print(dir)
@@ -42,6 +78,8 @@ for index, row in df.iterrows():
 
         if os.path.exists(old_location):
             shutil.copyfile(old_location, new_location)
+            temp = [old_location, new_location]
+            metafiles_moved.append(temp)
         else:
             print('')
             print(old_location)
@@ -57,7 +95,35 @@ for index, row in df.iterrows():
         new_location = row[1]
         if os.path.exists(old_location):
             os.rename(old_location, new_location)
+            temp = [old_location, new_location]
+            genomes_moved.append(temp)
         else:
             print('')
             print(old_location)
             print(new_location)
+
+wb = Workbook()
+
+sheet = wb.create_sheet('directories')
+del wb["Sheet"]
+cols = ["dir_created"]
+sheet.append(cols)
+for line_dir in dirs_created:
+    sheet.append([line_dir])
+style_sheet(sheet)
+
+sheet = wb.create_sheet('metafiles')
+cols = ["old_location", "new_location"]
+sheet.append(cols)
+for line_meta in metafiles_moved:
+    sheet.append(line_meta)
+style_sheet(sheet)
+
+sheet = wb.create_sheet('genomes')
+cols = ["old_location", "new_location"]
+sheet.append(cols)
+for line_genome in genomes_moved:
+    sheet.append(line_genome)
+style_sheet(sheet)
+
+wb.save("../../output/50_bilan_transfert_p2m.xlsx")
