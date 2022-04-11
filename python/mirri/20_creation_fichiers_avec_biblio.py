4f417b1dfb47a00b5f2f9a6d72e96959a7d9fe41
Martin  BOUTROUX <mboutrou@pasteur.fr>
2022-04-11 18:04:22 +0200

    Added geographic sheet for MIRRI.

--- python/mirri/20_creation_fichiers_avec_biblio.py
+++ python/mirri/20_creation_fichiers_avec_biblio.py
@@ -16,20 +16,22 @@ cursor.execute(open("../envoi_souches/mirri_biblio_par_id.sql", "r").read())
 cursor.execute(open("../envoi_souches/mirri.sql", "r").read())
 records = cursor.fetchall()
 
+
+# gestion des biblis
 f_biblis = open('../../output/bilan_biblis.csv', 'r', encoding="utf-8", newline='')
 biblis = csv.reader(f_biblis, delimiter=';')
 
 biblis_par_id = {}
 correspondances_biblis = []
-numero  = 0
+numero_bibli  = 0
 for elmt in biblis:
     ids_elmt = elmt[0]
     type_elmt = elmt[1]
     val_elmt = elmt[2]
 
     if not(type_elmt == "doi" or type_elmt == "pmid"):
-        numero += 1
-        list = [numero, val_elmt, '', '', elmt[3], elmt[4], elmt[5], '', elmt[6], elmt[7]]
+        numero_bibli += 1
+        list = [numero_bibli, val_elmt, '', '', elmt[3], elmt[4], elmt[5], '', elmt[6], elmt[7]]
         correspondances_biblis.append(list)
 
     for id_elmt in ids_elmt.split(','):
@@ -40,12 +42,34 @@ for elmt in biblis:
         if type_elmt == "doi" or type_elmt == "pmid":
             biblis_par_id[int_id].append(val_elmt)
         else:
-            biblis_par_id[int_id].append(numero)
-    
+            biblis_par_id[int_id].append(numero_bibli)
+
+# gestion des géographies
+cursor.execute(open("../envoi_souches/mirri_geography.sql", "r").read())
+records_pays = cursor.fetchall()
+
+pays_par_id = {}
+correspondances_pays = []
+numero_pays  = 0
+for record in records_pays:
+    ids_elmt = record[2]
+    pays = record[0]
+    lieu_precis = record[1]
+
+    numero_pays += 1
+    list = [numero_pays, pays, '', '', lieu_precis]
+    correspondances_pays.append(list)
+
+    for id_elmt in ids_elmt.split(','):
+        int_id = int(id_elmt)
+        pays_par_id[int_id] = numero_pays
+
+# gestion des strains
 rows_strains = []
 for record in records:
     xxx_id = record[0]
     row = [r for r in record[1:]]
+    row[30] = pays_par_id[xxx_id]
     refs = ""
     if xxx_id in biblis_par_id:
         for ref in biblis_par_id[xxx_id]:
@@ -65,5 +89,10 @@ f_literature = open('../../output/sheet_literature.csv', 'w', encoding="utf-8", 
 writer_literature = csv.writer(f_literature, delimiter=';')
 writer_literature.writerows(correspondances_biblis)
 f_literature.close()
+
+f_pays = open('../../output/sheet_pays.csv', 'w', encoding="utf-8", newline='')
+writer_pays = csv.writer(f_pays, delimiter=';')
+writer_pays.writerows(correspondances_pays)
+f_pays.close()
 
 f_biblis.close()
